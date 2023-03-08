fa5c07262b6fda97959b47a013c986254d831f1c
Imran Ahmed <9381772+manimirzaa@users.noreply.github.com>
2023-03-08 16:02:17 +0500

    Fix the bugs in migration and created some more models

--- app/RepairJob.php
+++ app/RepairJob.php
@@ -2,10 +2,16 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class RepairJob extends Model
 {
     use HasFactory;
+
+    public function service_type(): MorphOne
+    {
+        return $this->morphOne(ServiceType::class, 'serviceable');
+    }
 }
