813143d04de536acf47320d896a08feb9517840b
Imran Ahmed <9381772+manimirzaa@users.noreply.github.com>
2023-06-26 13:02:05 +0500

    Fixed Vehicle Management
    Fixed vehicle management

--- resources/views/email/customers.blade.php
+++ resources/views/email/customers.blade.php
@@ -1,12 +1,12 @@
 <x-mail::message>
 # Introduction
 
-You are recieving this email becuase you have been send an invitaion as customer against your Van enquiry.
+You are recieving this email becuase you have been sent an invitaion as customer against your Van enquiry.
 
 click on the button blow to fill the customer form
 
 <x-mail::button :url="env('FRONT_APP_URL') . '/user/register'">
-Fill the customer form
+    Fill the customer form
 </x-mail::button>
 
 Thanks,<br>
