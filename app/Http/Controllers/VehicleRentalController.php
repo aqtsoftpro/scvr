8f4232c7ea26a7a7d1da8a6fdd8e8778b6c8a022
Afzaal565 <afzaalhussain565@gmail.com>
2024-04-06 15:49:20 +0500

    few updates by afzaal

--- app/Http/Controllers/VehicleRentalController.php
+++ app/Http/Controllers/VehicleRentalController.php
@@ -28,7 +28,7 @@ class VehicleRentalController extends Controller
      */
     public function show(VanOut $vanOut)
     {
-        $vanout = $vanOut->load('customer', 'galleries', 'vehicle.status', 'vehicle.vehicle_type', 'location');
+        $vanout = $vanOut->load('customer', 'swapWith', 'galleries', 'vehicle.status', 'vehicle.vehicle_type', 'location');
 
         return response()->json($vanout);
 
