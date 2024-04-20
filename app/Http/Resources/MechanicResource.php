85d101b40e337591f668bbb8d784c1385396410d
Afzaal565 <afzaalhussain565@gmail.com>
2024-04-20 17:13:37 +0500

    mechanics bug fixed

--- app/Http/Resources/MechanicResource.php
+++ app/Http/Resources/MechanicResource.php
@@ -16,6 +16,7 @@ class MechanicResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'workshop_contact' => $this->workshop_contact,
             'workshop_address' => $this->workshop_address,
