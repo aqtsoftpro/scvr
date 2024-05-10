e305c495500399503fda838ec0fc92386391ac2c
Afzaal565 <afzaalhussain565@gmail.com>
2024-05-10 18:49:13 +0500

    new updates for reports

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -128,7 +128,7 @@ class ReportsController extends Controller
 
         foreach($vanouts as $vanout){
             $pnl[$index]['date'] = Carbon::parse($vanout->created_at)->format('d-m-Y');
-            $pnl[$index]['notes'] = 'Rented out ' . $vanout->vehicle?->vehicle_type->name .  ' with reg# '. $vanout->vehicle?->reg_plate_number;
+            $pnl[$index]['notes'] = 'Rented out ' . $vanout->vehicle?->vehicle_type?->name .  ' with reg# '. $vanout->vehicle?->reg_plate_number;
             $pnl[$index]['cost'] = $vanout->rental_amount;
             $pnl[$index]['tax'] = 0.00;
             $pnl[$index]['maintenance'] = 0.00;
